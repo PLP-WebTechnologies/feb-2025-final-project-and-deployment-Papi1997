17cb227af15d755b3279874b9684c3260a73f10f
Papi1997 <lordjarvis02@gmail.com>
2025-05-13 20:02:50 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function transferMoney() {
+    alert("Money transfer initiated!");
+}
+
+document.querySelector('#contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert("Thank you for contacting us!");
+});
